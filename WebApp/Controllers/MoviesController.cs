bd391527a3a380d3c045ee529fbc290e3e5e8b54
Omer Citik <omer.citik@hva.nl>
2021-05-02 17:56:41 +0200

    Tests with Controller

--- WebApp/Controllers/MoviesController.cs
+++ WebApp/Controllers/MoviesController.cs
@@ -10,10 +10,30 @@ namespace WebApp.Controllers
     public class MoviesController : Controller
     {
  
-        // GET: Movies/Random
+        // GET: Movies/Random, you can see it as route
         public ActionResult Random() {
             var movie = new Movie() { Name="Shrek!" };
             return View(movie);
         }
+
+        // GET: Movies/Edit
+        public ActionResult Edit(int id) {
+            return Content("id= "+id);
+        }
+
+        // GET: Movies index page
+        public ActionResult Index(int? pageIndex, string sortBy) {
+
+            if (!pageIndex.HasValue) {
+                pageIndex=1; // if no query parameter is passed set the default value
+            }
+            if (String.IsNullOrEmpty(sortBy)) {
+                sortBy="Name"; // if no query parameter is passed set the default value
+            }
+
+            return Content(String.Format("pageIndex={0} & sortBy={1} ", pageIndex, sortBy));
+        }
+
+
     }
 }
